586f5c1a1030ca06fd5f2ca1cd26924895c7aa86
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-16 11:53:40 +0900

    Study js principle

---
+++ momentum/app.js
@@ -0,0 +1,18 @@
+const toDo = ["have fun", "see movie", "eat helth food"];
+console.log(toDo);
+
+toDo.push("sleep");
+console.log(toDo);
+
+const std = {
+    name: "hwido",
+    loving: true,
+    money: "saving",
+};
+console.log(std);
+
+std.money = "saved";
+console.log(std);
+
+std.authority = "YEs";
+console.log(std);

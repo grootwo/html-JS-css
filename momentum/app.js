a761031ca9167154091db17e8f31bb009f1a3837
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-17 11:35:44 +0900

    Fix login & greeting interaction

--- momentum/app.js
+++ momentum/app.js
@@ -10,15 +10,21 @@ function handleLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASS);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
-    savedUsername = username;
+    paintGreeting(username);
 }
 
+function paintGreeting(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASS);
+}
+
+
+// username을 확인하여 form이나 문구 표시하기
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
     loginForm.classList.remove(HIDDEN_CLASS);
     loginForm.addEventListener("submit", handleLoginSubmit);
 } else {
-    greeting.innerText = `Hello ${savedUsername}`;
-    greeting.classList.remove(HIDDEN_CLASS);
+    paintGreeting(savedUsername);
 }

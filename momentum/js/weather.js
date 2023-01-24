82cc7c07fd1cfb157dc856915d1c0b30c59ff983
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-24 19:42:42 +0900

    Add weather API

---
+++ momentum/js/weather.js
@@ -0,0 +1,19 @@
+const weather = document.querySelector(".weather span:nth-child(1)");
+const city = document.querySelector(".weather span:nth-child(2)");
+
+function geoSuccess(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    console.log("you're in", latitude, longitude);
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`;
+    fetch(url).then((response) => response.json()).then((data) => {
+        city.innerText = date.name;
+        weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+    });
+}
+
+function geoError() {
+    alert("Can't find your geolocation's information")
+}
+
+navigator.geolocation.getCurrentPosition(geoSuccess, geoError)

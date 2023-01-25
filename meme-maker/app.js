d1ef8bb01f3b17142b7e12989f632c92df8f97ca
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-25 10:54:22 +0900

    Draw rects

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -3,3 +3,18 @@ const ctx = canvas.getContext("2d");
 
 canvas.width = 600;
 canvas.height = 600;
+
+ctx.rect(0, 0, 100, 100);
+ctx.rect(500, 500, 100, 100);
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(100, 100, 120, 120);
+ctx.rect(380, 380, 120, 120);
+ctx.fillStyle = "gray";
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(220, 220, 160, 160);
+ctx.fillStyle = "cornflowerblue";
+ctx.fill();

be2db5099e4591fd179153fd48278efa306c016c
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-26 17:19:47 +0900

    Add hidden class

--- my-home-page/js/userComment.js
+++ my-home-page/js/userComment.js
@@ -1,12 +1,29 @@
 const userComment = document.querySelector(".user-comment");
 const userCommentForm = userComment.querySelector("form");
 const userCommentInput = userComment.querySelector("input");
-const userCommentMessage = userComment.querySelector("span");
+const userCommentSpan = userComment.querySelector("span");
 
+const USER_COMMENT_KEY = "userComment";
+const HIDDEN_CLASS = "hidden";
 
 function userCommentSubmit(event) {
     event.preventDefault();
-    userCommentMessage.innerText = userCommentInput.value;
+    const userCommentMessage = userCommentInput.value;
+    localStorage.setItem(USER_COMMENT_KEY, userCommentMessage);
+    userCommentSpan.innerText = userCommentMessage;
 }
+// function paintUserComment() {
+//     userCommentSpan.classList.remove(HIDDEN_CLASS);
+//     userCommentSpan.innerText = savedUserComment;
+// }
 
 userCommentForm.addEventListener("submit", userCommentSubmit);
+
+const savedUserComment = localStorage.getItem(USER_COMMENT_KEY);
+
+if (savedUserComment !== null) {
+    userCommentSpan.classList.remove(HIDDEN_CLASS);
+    userCommentSpan.innerText = savedUserComment;
+} else {
+    userCommentForm.classList.remove(HIDDEN_CLASS);
+}

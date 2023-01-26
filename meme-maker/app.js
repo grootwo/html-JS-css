b1a1f08067d5444eea2c393dbc70d7bf5f85eb16
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-26 10:12:11 +0900

    Draw line and arc

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -18,3 +18,20 @@ ctx.beginPath();
 ctx.rect(220, 220, 160, 160);
 ctx.fillStyle = "cornflowerblue";
 ctx.fill();
+
+ctx.beginPath();
+ctx.moveTo(380, 150);
+ctx.lineTo(450, 50);
+ctx.lineTo(520, 150);
+ctx.lineTo(380, 120);
+ctx.strokeStyle = "blue";
+ctx.lineWidth = 1;
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(200, 400, 100, 0, 1.5 * Math.PI);
+ctx.stroke();
+
+ctx.beginPath();
+ctx.arc(150, 450, 50, 1 * Math.PI, 0.5 * Math.PI);
+ctx.stroke();

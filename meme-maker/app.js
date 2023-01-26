a85edea5830e1b34f1c7dbac3d728027991ea4cf
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-26 11:29:53 +0900

    Make range editing line width

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -1,13 +1,16 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const lineWidth = document.querySelector(".line-width");
 
 canvas.width = 600;
 canvas.height = 600;
+ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
 
 function startPainting(event) {
     isPainting = true;
+    ctx.beginPath();
 }
 
 function cancelPainting() {
@@ -23,8 +26,13 @@ function onMouseMove(event) {
     }
 }
 
+function lineWidthChange() {
+    console.dir(lineWidth.value);
+    ctx.lineWidth = lineWidth.value;
+}
 
 canvas.addEventListener("mousemove", onMouseMove);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+lineWidth.addEventListener("change", lineWidthChange);

d5a5a879fc91c6c17fbc220ae345ed406e185637
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-26 11:14:30 +0900

    Make mouse painting

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -4,11 +4,27 @@ const ctx = canvas.getContext("2d");
 canvas.width = 600;
 canvas.height = 600;
 
-function onMouseMove(event) {
-    ctx.beginPath();
-    ctx.moveTo(600, 600);
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke();
+let isPainting = false;
+
+function startPainting(event) {
+    isPainting = true;
 }
 
+function cancelPainting() {
+    isPainting = false;
+}
+
+function onMouseMove(event) {
+    if (isPainting) {
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+    } else {
+        ctx.moveTo(event.offsetX, event.offsetY);
+    }
+}
+
+
 canvas.addEventListener("mousemove", onMouseMove);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseleave", cancelPainting);

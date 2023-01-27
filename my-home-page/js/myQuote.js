b38f88cf18fb2efb91f6b49292c885f263422b77
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-27 17:23:29 +0900

    Add quotes random showing and submitting

--- my-home-page/js/myQuote.js
+++ my-home-page/js/myQuote.js
@@ -1,8 +1,13 @@
 const myQuote = document.querySelector(".my-quote__message");
 const quote = myQuote.querySelector(".my-quote__quote");
 const info = myQuote.querySelector(".my-quote__info");
+const quoteForm = document.querySelector(".second-layout .input-form");
+const quoteInput = quoteForm.querySelector("input:first-child");
+const infoInput = quoteForm.querySelector("input:last-child");
 
-myQuotes = [
+const QUOTES_KEY = "myQuotes";
+
+let myQuotes = [
     {
         quote: "일찍, 많이 실패하자!",
         info: "23.01.27.금",
@@ -15,8 +20,30 @@ myQuotes = [
         quote: "모르고 넘어가는 것이 더 부끄러운 일이다.",
         info: "그러니 도전하자.",
     },
-]
+];
 
-const chosenQuote = myQuotes[Math.floor(Math.random() * myQuotes.length)];
-quote.innerText = chosenQuote.quote;
-info.innerText = chosenQuote.info;
+function quoteSubmit(event) {
+    event.preventDefault();
+    const newQuote = {
+        quote: quoteInput.value,
+        info: infoInput.value,
+    };
+    myQuotes.push(newQuote);
+    saveQuotes();
+}
+function saveQuotes() {
+    localStorage.setItem(QUOTES_KEY, JSON.stringify(myQuotes));
+}
+function loadQuotes() {
+    myQuotes = JSON.parse(localStorage.getItem(QUOTES_KEY));
+}
+function paintQuote() {
+    loadQuotes();
+    const chosenQuote = myQuotes[Math.floor(Math.random() * myQuotes.length)];
+    quote.innerText = chosenQuote.quote;
+    info.innerText = chosenQuote.info;
+}
+
+quoteForm.addEventListener("submit", quoteSubmit);
+
+paintQuote();

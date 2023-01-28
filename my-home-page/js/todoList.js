c122845b828bc60c105cba40c441322bcdb23876
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-28 23:21:17 +0900

    Save todos in localStorage and show items

--- my-home-page/js/todoList.js
+++ my-home-page/js/todoList.js
@@ -2,23 +2,43 @@ const todoList = document.querySelector(".todo-list__list");
 const todoForm = document.querySelector(".todo-list .input-form");
 const todoInput = todoForm.querySelector("input");
 
+let todos = [];
+const TODOS_KEY = "todos";
+
 function addNewTodo(event) {
     event.preventDefault();
-    const newTodo = todoInput.value;
+    const newTodo = {
+        text: todoInput.value,
+        id: Date.now(),
+    }
     todoInput.value = "";
-    console.log(newTodo);
-    createNewTodo(newTodo);
+    todos.push(newTodo);
+    showNewTodo(newTodo);
+    saveTodos();
 }
-function createNewTodo(newTodo) {
+function showNewTodo(newTodo) {
     const todoSpan = document.createElement("span");
-    todoSpan.innerText = newTodo;
+    todoSpan.innerText = newTodo.text;
     const todoBtn = document.createElement("Button");
     todoBtn.innerText = "✖️";
     const todoLi = document.createElement("li");
     todoLi.className = "todo";
+    todoLi.id = newTodo.id;
     todoLi.appendChild(todoSpan);
     todoLi.appendChild(todoBtn);
     todoList.appendChild(todoLi);
 }
+function saveTodos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
+}
 
 todoForm.addEventListener("submit", addNewTodo);
+
+const savedTodos = JSON.parse(localStorage.getItem(TODOS_KEY));
+
+if (saveTodos !== null) {
+    todos = savedTodos;
+    todos.forEach(item => {
+        showNewTodo(item);
+    })
+}

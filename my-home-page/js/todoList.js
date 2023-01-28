ab70cff8a43b1da929fa18a5cf3ed074cd288dc6
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-28 23:26:48 +0900

    Make todos delete

--- my-home-page/js/todoList.js
+++ my-home-page/js/todoList.js
@@ -21,6 +21,7 @@ function showNewTodo(newTodo) {
     todoSpan.innerText = newTodo.text;
     const todoBtn = document.createElement("Button");
     todoBtn.innerText = "✖️";
+    todoBtn.addEventListener("click", deleteTodo);
     const todoLi = document.createElement("li");
     todoLi.className = "todo";
     todoLi.id = newTodo.id;
@@ -31,8 +32,17 @@ function showNewTodo(newTodo) {
 function saveTodos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(todos));
 }
+function deleteTodo(event) {
+    const selectedLi = event.target.parentElement;
+    selectedLi.remove();
+    todos = todos.filter(item => {
+        return item.id !== parseInt(selectedLi.id)
+    });
+    saveTodos();
+}
 
 todoForm.addEventListener("submit", addNewTodo);
+
 
 const savedTodos = JSON.parse(localStorage.getItem(TODOS_KEY));
 

81a6897f0e6fd8af4d7c4f2df1286a40b9d9b088
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-29 13:51:44 +0900

    Fix when no img

--- my-home-page/js/myImg.js
+++ my-home-page/js/myImg.js
@@ -3,17 +3,21 @@ const myImgImg = myImg.querySelector("img");
 
 let myImgs = [
     "https://blog.kakaocdn.net/dn/ccW5UN/btrbWd3iYGl/xyD0biW4EXeSfltdKu2lv0/img.jpg",
-    "https://blog.kakaocdn.net/dn/INMNW/btrh5Yd46QG/2hcojqZe0D118hKUlDxnfk/img.jpg",
     "https://blog.kakaocdn.net/dn/cSZ5WZ/btrb1q8qzPs/X78yP3xYFgGC6IiITxp1KK/img.jpg",
-]
+    "https://blog.kakaocdn.net/dn/INMNW/btrh5Yd46QG/2hcojqZe0D118hKUlDxnfk/img.jpg",
+];
 let chosenImg;
 let chosenImgIndex;
 
 function setRandomImg() {
-    chosenImg = myImgs[Math.floor(Math.random() * myImgs.length)];
-    chosenImgIndex =  myImgs.indexOf(chosenImg);
-    console.log(chosenImg, chosenImgIndex);
-    myImgImg.src = chosenImg;
+    if (myImgs.length !== 0) {
+        chosenImg = myImgs[Math.floor(Math.random() * myImgs.length)];
+        chosenImgIndex =  myImgs.indexOf(chosenImg);
+        console.log(chosenImg, chosenImgIndex);
+        myImgImg.src = chosenImg;
+    } else {
+        myImgImg.src = "https://img.freepik.com/premium-vector/checkered-geometric-vector-background-with-black-gray-tile-transparent-grid-empty-layer_501045-1220.jpg?w=2000";
+    }
 }
 
 setRandomImg();

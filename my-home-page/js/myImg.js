00470ff630661b4d1be8cf763bd16463545be0e6
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-29 15:32:08 +0900

    Make my imgs' btn active

--- my-home-page/js/myImg.js
+++ my-home-page/js/myImg.js
@@ -2,6 +2,9 @@ const myImg = document.querySelector(".my-img");
 const myImgImg = myImg.querySelector("img");
 const myImgForm = myImg.querySelector(".input-form");
 const myImgInput = myImgForm.querySelector("input");
+const myImgDeleteBtn = document.querySelector(".my-img__btns button:nth-child(2)");
+const myImgLeftBtn = document.querySelector(".my-img__btns button:nth-child(1)");
+const myImgRightBtn = document.querySelector(".my-img__btns button:nth-child(3)");
 
 const IMGS_KEY = "myImgs";
 
@@ -11,14 +14,12 @@ let myImgs = [
     "https://blog.kakaocdn.net/dn/INMNW/btrh5Yd46QG/2hcojqZe0D118hKUlDxnfk/img.jpg",
 ];
 let chosenImg;
-let chosenImgIndex;
 
 function setRandomImg() {
     loadImgs();
     if (myImgs.length !== 0) {
         chosenImg = myImgs[Math.floor(Math.random() * myImgs.length)];
-        chosenImgIndex =  myImgs.indexOf(chosenImg);
-        console.log(chosenImg, chosenImgIndex);
+        console.log(chosenImg, myImgs.indexOf(chosenImg));
         myImgImg.src = chosenImg;
     } else {
         myImgImg.src = "https://img.freepik.com/premium-vector/checkered-geometric-vector-background-with-black-gray-tile-transparent-grid-empty-layer_501045-1220.jpg?w=2000";
@@ -37,7 +38,36 @@ function saveImgs() {
 function loadImgs() {
     myImgs = JSON.parse(localStorage.getItem(IMGS_KEY));
 }
+function deleteImg() {
+    console.log("delete");
+    myImgs = myImgs.filter(item => {
+        return item !== chosenImg
+    })
+    saveImgs();
+    setRandomImg();
+}
+function getLeftImg() {
+    console.log("left");
+    if (myImgs.indexOf(chosenImg) !== 0) {
+        chosenImg = myImgs[myImgs.indexOf(chosenImg) - 1];
+    } else {
+        chosenImg = myImgs[myImgs.length - 1];
+    }
+    myImgImg.src = chosenImg;
+}
+function getRightImg() {
+    console.log("right");
+    if (myImgs.indexOf(chosenImg) !== (myImgs.length - 1)) {
+        chosenImg = myImgs[myImgs.indexOf(chosenImg) + 1];
+    } else {
+        chosenImg = myImgs[0];
+    }
+    myImgImg.src = chosenImg;
+}
 
 myImgForm.addEventListener("submit", submitImg);
+myImgDeleteBtn.addEventListener("click", deleteImg);
+myImgLeftBtn.addEventListener("click", getLeftImg);
+myImgRightBtn.addEventListener("click", getRightImg);
 
 setRandomImg();

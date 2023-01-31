fc0e06fd2384a13622cacc56297d56790d51ad77
leegaeun <82134672+soaringwave@users.noreply.github.com>
2023-01-31 11:24:18 +0900

    Make fill mode active

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -3,12 +3,14 @@ const ctx = canvas.getContext("2d");
 const lineWidth = document.querySelector(".line-width");
 const color = document.querySelector(".color");
 const colorOptions = Array.from(document.getElementsByClassName("color-option"));
+const modeBtn = document.querySelector(".mode-btn");
 
 canvas.width = 600;
 canvas.height = 600;
 ctx.lineWidth = lineWidth.value;
 
 let isPainting = false;
+let isFilling = false;
 
 function startPainting(event) {
     isPainting = true;
@@ -28,6 +30,12 @@ function onMouseMove(event) {
     }
 }
 
+function onCanvasClick() {
+    if (isFilling) {
+        ctx.fillRect(0, 0, 600, 600);
+    }
+}
+
 function lineWidthChange(event) {
     ctx.lineWidth = event.target.value;
 }
@@ -44,12 +52,34 @@ function onColorClick(event) {
     ctx.fillStyle = chosenColor;
 }
 
+function onChangeMode() {
+    // 왜 이게 불필요한지 알아야 함
+    // if (isPainting) {
+    //     isPainting = false;
+    //     isFilling = true;
+    //     modeBtn.innerText = "Draw";
+    // } else {
+    //     isPainting = true;
+    //     isFilling = false;
+    //     modeBtn.innerText = "Fill";
+    // }
+    if (isFilling) {
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    } else {
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
 canvas.addEventListener("mousemove", onMouseMove);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
 lineWidth.addEventListener("change", lineWidthChange);
 color.addEventListener("change", onColorChange);
 colorOptions.forEach(item => {
     item.addEventListener("click", onColorClick);
 })
+modeBtn.addEventListener("click", onChangeMode);
